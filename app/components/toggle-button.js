7040b55faabdfc8edc281500711ab388afd20b09
Sergiu Cozma <lssjbrolli@gmail.com>
2018-03-26 17:25:37 +0300

    frontend: make toggle-button component more generic 	  reset button state in case of error

--- app/components/toggle-button.js
+++ app/components/toggle-button.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { capitalize } from '@ember/string';
 
 export default Component.extend({
   tagName: '',
@@ -7,12 +8,29 @@ export default Component.extend({
   flashMessages: service(),
 
   actions: {
-    toggleObj(id) {
-      const model = this.get('store').peekRecord('user', id);
-      model.toggleProperty('active');
+    toggleObj(obj, prop) {
+      const modelName = obj.constructor.modelName;
+      const model = this.get('store').peekRecord(modelName, obj.id);
+
+      model.toggleProperty(prop);
+
       model
         .save()
-        .then(this.flashMessages.success('User successfully updated!'));
+        .then(
+          () =>
+            this.flashMessages.success(
+              `${capitalize(modelName)} was successfully update!`
+            ),
+          () => this._failedUpdate(model, modelName)
+        )
+        .catch(() => this._failedUpdate(model, modelName));
     }
+  },
+
+  _failedUpdate(model, modelName) {
+    model.rollbackAttributes();
+    this.flashMessages.error(
+      `There was an error while updating the ${modelName}!`
+    );
   }
 });
