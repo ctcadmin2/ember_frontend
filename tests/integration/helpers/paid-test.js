b1cefaba4a98b2a698ce9d472b5baddab81463fa
Sergiu Cozma <lssjbrolli@gmail.com>
2019-01-29 19:11:08 +0200

    frontend: run qunit-dom-codemod

--- tests/integration/helpers/paid-test.js
+++ tests/integration/helpers/paid-test.js
@@ -10,12 +10,12 @@ module('Integration | Helper | paid', function(hooks) {
   test('it renders', async function(assert) {
     await render(hbs`{{paid true}}`);
 
-    assert.equal(this.element.textContent.trim(), 'check icon');
+    assert.dom(this.element).hasText('check icon');
   });
   // if false show X icon
   test('it renders', async function(assert) {
     await render(hbs`{{paid false}}`);
 
-    assert.equal(this.element.textContent.trim(), 'close icon');
+    assert.dom(this.element).hasText('close icon');
   });
 });
