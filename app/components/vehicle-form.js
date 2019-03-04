b16ba75eb426436736dd0e5fb0622217033320a0
Sergiu Cozma <lssjbrolli@gmail.com>
2019-03-04 17:33:30 +0200

    frontend: add categories to vehicle form

--- app/components/vehicle-form.js
+++ app/components/vehicle-form.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { set } from '@ember/object';
 
 export default Component.extend({
+  store: service(),
+  prefs: null,
+  init() {
+    this._super(...arguments);
+    this.store.findRecord('settings', 1).then(r => set(this, 'prefs', r.main));
+  }
 });
