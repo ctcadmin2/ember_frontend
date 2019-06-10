d971004d1eb0d8b8f0dd7ea03a48d2bf8f6973f1
Sergiu Cozma <lssjbrolli@gmail.com>
2019-06-11 00:28:39 +0300

    update ember

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,61 @@
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
+    ecmaVersion: 2018,
+    sourceType: "module"
   },
-  plugins: ['ember', 'hbs', 'compat', 'html'],
+  plugins: ["ember", "hbs", "compat", "html"],
   extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:compat/recommended',
+    "eslint:recommended",
+    "plugin:ember/recommended",
+    "plugin:compat/recommended"
   ],
   env: {
-    browser: true,
+    browser: true
   },
   rules: {
-    'ember/named-functions-in-promises': [
+    "ember/named-functions-in-promises": [
       2,
       {
-        allowSimpleArrowFunction: true,
-      },
+        allowSimpleArrowFunction: true
+      }
     ],
-    'hbs/check-hbs-template-literals': 2,
+    "hbs/check-hbs-template-literals": 2
     // 'ember/no-jquery': 2 TODO can't remove yet
   },
   overrides: [
     // node files
     {
       files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'ember-cli-build.js',
-        'config/**/*.js',
-        'lib/*/index.js',
-        'server/**/*.js',
+        ".eslintrc.js",
+        ".template-lintrc.js",
+        "ember-cli-build.js",
+        "testem.js",
+        "ember-cli-build.js",
+        "config/**/*.js",
+        "lib/*/index.js",
+        "server/**/*.js"
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015,
+        sourceType: "script",
+        ecmaVersion: 2015
       },
       env: {
         browser: false,
-        node: true,
+        node: true
       },
-    },
-  ],
+      plugins: ["node"],
+      rules: Object.assign(
+        {},
+        require("eslint-plugin-node").configs.recommended.rules,
+        {
+          // add your custom rules and overrides for node files here
+
+          // this can be removed once the following is fixed
+          // https://github.com/mysticatea/eslint-plugin-node/issues/77
+          "node/no-unpublished-require": "off"
+        }
+      )
+    }
+  ]
 };
