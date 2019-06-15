3a70a06af5ed55ccaeaba8e277e5787f8e2afaf0
Sergiu Cozma <lssjbrolli@gmail.com>
2019-06-15 20:23:46 +0300

    move nav links from top to side

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,6 @@
-import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
-import { set } from '@ember/object';
+import Controller from "@ember/controller";
+import { inject as service } from "@ember/service";
+import { set, computed } from "@ember/object";
 
 // Eslint rules
 /* eslint ember/alias-model-in-controller: "off"*/
@@ -10,12 +10,19 @@ export default Controller.extend({
   router: service(),
   currentUser: service(),
   intl: service(),
+  fullName: computed("currentUser", function() {
+    if (this.currentUser.user) {
+      let fName = this.currentUser.user.firstName;
+      let lName = this.currentUser.user.lastName;
+      return `${fName} ${lName}`;
+    }
+  }),
   actions: {
     invalidateSession() {
       this.session.invalidate();
     },
     changeLocale(locale) {
-      return set(this.intl, 'locale', locale);
+      return set(this.intl, "locale", locale);
     }
   }
 });
